f02c49ac6c417a9388f35845f14702dfa2514c2b
jiangxiaokun <jiangxiaokun@bytedance.com>
2020-10-12 14:59:42 +0800

    feat: use stories reg succeed

--- storybook-starter/.storybook/main.js
+++ storybook-starter/.storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx"],
-  addons: [
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
     "@storybook/addon-docs",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-  ],
-};
+    "@storybook/addon-essentials"
+  ]
+}
